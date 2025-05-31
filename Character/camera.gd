826cf82b55d05ca8937d54ab0ab961d6d3f901da
MegaModular <76797049+MegaModular@users.noreply.github.com>
2025-05-31 16:11:10 -0700

    Player controller completed

---
+++ Character/camera.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+var following : Object = null
+
+@onready var player = $"../Character"
+
+
+func _process(delta: float) -> void:
+	if following != null:
+		position = lerp(position, following.position, 0.1) 
+		return
+	position = lerp(position, player.position, 0.1)
---
+++ Character/character.gd
@@ -0,0 +1,99 @@
+extends CharacterBody2D
+
+#Editable
+var move_speed = 250
+var rotation_speed = 5.0
+
+var maxShieldHealth = 100.0
+var shieldRegenRate = 10.0 #per second
+
+#States
+var ableToAttack : bool = true
+var isDefending : bool = false
+var shieldUp : bool = false
+
+#backend
+var shieldHealth : float = 0
+
+var currentFocus = Vector2.ZERO
+var direction = Vector2.ZERO
+var raisingShield :float = 0
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	debugFunc()
+	
+	calculateMovement()
+	pointStuffAtCursor(delta)
+	shieldLogic(delta)
+	
+	if Input.is_action_just_pressed("lmb"):
+		attack()
+	
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
+
+func pointStuffAtCursor(delta) -> void:
+	var mouse_pos = get_global_mouse_position()
+	var to_mouse = (mouse_pos - global_position).normalized()
+	var current_rotation = $CursorHolder.rotation
+	var target_rotation = to_mouse.angle()
+	$CursorHolder.rotation = lerp_angle(current_rotation, target_rotation, rotation_speed * delta)
+
+#Handles player movement
+func calculateMovement() -> void:
+	direction = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		direction += Vector2(0, -1)
+	if Input.is_action_pressed("down"):
+		direction += Vector2(0, 1)
+	if Input.is_action_pressed("left"):
+		direction += Vector2(-1, 0)
+	if Input.is_action_pressed("right"):
+		direction += Vector2(1, 0)
+	
+	velocity = direction.normalized() * move_speed
+	move_and_slide()
+
+func attack() -> void:
+	if ableToAttack && !isDefending:
+		$AnimationPlayer.play('slash')
+		$SlashCooldown.start()
+		ableToAttack = false
+
+func shieldLogic(delta) -> void:
+	#shield regen
+	if !isDefending:
+		if shieldHealth < maxShieldHealth:
+			shieldHealth += shieldRegenRate * delta
+	clamp(shieldHealth, 0, maxShieldHealth)
+	
+	#raising shield up
+	if ableToAttack && Input.is_action_pressed("rmb"):
+		isDefending = true
+		raisingShield = lerp(raisingShield, 1.0, 4 *delta)
+	#lowering shield
+	else:
+		raisingShield = lerp(raisingShield, 0.0, 4*delta)
+		shieldUp = false
+		if raisingShield < 0.1 && isDefending:
+			isDefending = false
+	#Shield is up
+	if raisingShield > 0.87:
+		shieldUp = true
+		$CursorHolder/DefendArea/CollisionShape2D.disabled = false
+	
+	if(raisingShield < 0.87):
+		$CursorHolder/Shield.modulate = Color(255, 255, 255, 0)
+	else: 
+		$CursorHolder/Shield.modulate = Color(255, 255, 255, 255)
+
+func _on_slash_cooldown_timeout() -> void:
+	ableToAttack = true
+
+func debugFunc():
+	$debuglable.text = str(maxShieldHealth)
+	$debuglable2.text = str(shieldHealth)
+	$debuglable3.text = str(isDefending)
